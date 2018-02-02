00ae1ba09e01fc916508bb8a9eda5a5f9af97459
Michiel PAUW <mpauw@e3r8p19.42.fr>
2018-02-02 15:22:03 +0100

    Problem of distance solved

--- raytracer.c
+++ raytracer.c
@@ -6,19 +6,19 @@
 /*   By: mpauw <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 13:38:46 by mpauw             #+#    #+#             */
-/*   Updated: 2018/02/02 12:08:52 by mpauw            ###   ########.fr       */
+/*   Updated: 2018/02/02 15:14:48 by mpauw            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-static t_object	*get_visible_object(double *s_value, t_3v dir, int src_id,
+static t_object	*get_visible_object(double *s_value, t_3v dir,
 		t_list *objects)
 {
 	double		tmp;
+	double		size;
 	t_object	*obj;
 	t_object	*visible_object;
-	t_source	*src;
 	t_3v		tmp_dir;
 
 	visible_object = NULL;
@@ -26,11 +26,10 @@ static t_object	*get_visible_object(double *s_value, t_3v dir, int src_id,
 	{
 		obj = (t_object *)objects->content;
 		tmp_dir = get_dir(dir, obj->rotation);
-		if (src_id == 0)
-			src = &(obj->rel_cam);
-		else
-			src = get_source(src_id, obj->rel_lights);
-		tmp = obj->f(obj, tmp_dir, src->origin);
+		size = ft_get_3v_size(tmp_dir);
+		if (size == 0)
+			error(5);
+		tmp = obj->f(obj, tmp_dir, (obj->rel_cam).origin);
 		if (tmp > 0 && tmp < *s_value)
 		{
 			*s_value = tmp;
@@ -52,11 +51,9 @@ static void		get_pixel_value(t_scene *scene, t_3v pixel, int *pix_v)
 	tmp = scene->objects;
 	s_value = MAX_S_VALUE;
 	cam = scene->camera;
-	(pixel.v)[0] -= ((cam.origin).v)[0];
-	(pixel.v)[1] -= ((cam.origin).v)[1];
-	(pixel.v)[2] -= ((cam.origin).v)[2];
+
 	change_dir(&pixel, (scene->camera).rotation);
-	obj = get_visible_object(&s_value, pixel, 0, scene->objects);
+	obj = get_visible_object(&s_value, pixel, scene->objects);
 	scene->objects = tmp;
 	if (obj)
 	{
@@ -85,7 +82,7 @@ void			raytracer(t_event *event, t_scene *scene)
 		while (++j < img->width)
 		{
 			pix_val = BG_COLOR;
-			(pixel.v)[0] = 0.0;
+			(pixel.v)[0] = -1000.0;
 			(pixel.v)[1] = (double)(j - img->width / 2.0);
 			(pixel.v)[2] = (double)(img->height / 2.0 - i);
 			get_pixel_value(scene, pixel, &pix_val);

1db03b717c8d3a07e7b7d529f433e85b1980125a
Michiel PAUW <mpauw@e1r8p13.42.fr>
2018-06-26 10:12:40 +0200

    Push

--- set_render.c
+++ set_render.c
@@ -6,13 +6,13 @@
 /*   By: mpauw <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/30 15:49:09 by mpauw             #+#    #+#             */
-/*   Updated: 2018/06/25 13:19:43 by mpauw            ###   ########.fr       */
+/*   Updated: 2018/06/26 10:12:29 by mpauw            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-static void	set_values_render_2(t_scene *scene, char *s, char *value)
+static void	set_values_render_3(t_scene *scene, char *s, char *value)
 {
 	if (ft_strncmp(s, "ambient", 7) == 0)
 		scene->ambient = ft_atod(value);
@@ -29,15 +29,11 @@ static void	set_values_render_2(t_scene *scene, char *s, char *value)
 	}
 }
 
-static void	set_value_render_2(t_scene *scene, char *s, char *value)
+static void	set_values_render_2(t_scene *scene, char *s, char *value)
 {
 	double	*tmp;
 
-	if (ft_strncmp(s, "ambient", 7) == 0 || ft_strncmp(s, "grain", 5) == 0 ||
-		ft_strncmp(s, "wait", 4) == 0
-		|| ft_strncmp(s, "reflection", 10) == 0)
-		set_values_render_2(scene, s, value);
-	else if (ft_strncmp(s, "filter", 6) == 0)
+	if (ft_strncmp(s, "filter", 6) == 0)
 		scene->filter = ft_atoi(value);
 	else if (ft_strncmp(s, "dimensions", 10) == 0)
 	{
@@ -56,6 +52,7 @@ static void	set_value_render_2(t_scene *scene, char *s, char *value)
 		}
 		free(tmp);
 	}
+	set_values_render_3(scene, s, value);
 }
 
 static void	set_values_render(t_scene *scene, char *s, char *value)
@@ -67,7 +64,7 @@ static void	set_values_render(t_scene *scene, char *s, char *value)
 			error(1);
 		ft_strcpy(scene->name, (const char *)value);
 	}
-	set_value_render_2(scene, s, value);
+	set_values_render_2(scene, s, value);
 }
 
 void		set_render(t_scene *scene, int fd)
